f564c2605d87ad910434660adfbb5890ffc9b8ae
Tyler Porter <tyler.porter@arrayhealth.com>
2015-07-10 08:38:06 -0700

    update install script

--- init/install.sh
+++ init/install.sh
@@ -1,13 +1,16 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install caskroom/cask/brew-cask
 brew tap caskroom/versions
+brew cask install atom
 brew cask install google-chrome
 brew cask install google-chrome-canary
+brew cask install charles
 brew cask install dash
 brew cask install dropbox
 brew cask install evernote
 brew cask install jadengeller-helium
 brew cask install firefox
+brew cask install gitter
 brew cask install iterm2
 brew cask install jadengeller-helium
 brew cask install jumpcut
@@ -15,8 +18,14 @@ brew cask install sublime-text3
 brew cask install slack
 brew cask install spectacle
 brew cask install tower
-brew cask install charles
+brew cask install viber
 brew cask install virtualbox
 brew cask install ynab
 
 npm install -g gulp grunt-cli jshint jscs yo
+
+apm install atom-beautify
+apm install angularjs-styleguide-snippets
+apm install editorconfig
+apm install linter
+apm install linter jshint
