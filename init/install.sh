7bbb9abb3e9d6963d69f88fe86a0dc4041b3690a
Tyler Porter <tyler.porter@arrayhealth.com>
2015-08-13 16:33:06 -0700

    Add trash to install.sh

--- init/install.sh
+++ init/install.sh
@@ -1,4 +1,5 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install trash
 brew install caskroom/cask/brew-cask
 brew tap caskroom/versions
 brew cask install atom
